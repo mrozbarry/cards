d90e9f3842c216d47c0b8d59c5e3668da282509b
Alex <alex.barry@gmail.com>
2016-11-13 05:45:15 -0500

    Sharing dev and prod firebase environments for now, I may or may not change this in the future.

--- src/cards/lib/firebase.js
+++ src/cards/lib/firebase.js
@@ -7,9 +7,9 @@ export const development = {
 }
 
 export const production = {
-  apiKey: "TODO",
-  authDomain: "TODO",
-  databaseURL: "TODO",
-  storageBucket: "TODO",
-  messagingSenderId: "TODO"
+  apiKey: "AIzaSyDe3NDxBOqLqoPs9VXC0YdTWWOtzdt3-9E",
+  authDomain: "nodeknockout-cards.firebaseapp.com",
+  databaseURL: "https://nodeknockout-cards.firebaseio.com",
+  storageBucket: "nodeknockout-cards.appspot.com",
+  messagingSenderId: "670999733708"
 }
